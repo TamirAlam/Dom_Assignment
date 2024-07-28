0d4fff8c8130b39a1c82523d9a5a390931352906
Tamir Alam <tamiralam2@gmai.com>
2024-07-28 16:06:32 +0530

    index.html file is added

---
+++ script.js
@@ -0,0 +1,14 @@
+var divElem=document.getElementById("box");
+
+divElem.style.color="blue";
+
+divElem.style.backgroundColor="yellow";
+
+divElem.style.padding="10px";
+
+divElem.style.fontSize="18px";
+
+divElem.style.fontWeight="bold";
+
+divElem.style.height="200px";
+divElem.style.width="300px";
